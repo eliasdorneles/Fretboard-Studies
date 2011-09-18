4963050c4ba1fdb9ad774ce618df9a849266dcea
Elias Dorneles <eliasdorneles@gmail.com>
2011-09-18 01:09:50 -0300

    * updated script

--- update_page.sh
+++ update_page.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
-[ -f fretboard.html ] || { echo "dir gh-pages doesn't exist! aborting..."; exit 1; }
-echo -n "Generating page... "
+die(){
+    echo "Erro: $*"
+    exit 1
+}
+[ -f fretboard.html ] || die "dir gh-pages doesn't exist! aborting..."
+
+gitstatus=`git status -s`
+[ "x$gitstatus" == "x" ] || die "there are changes to be commited!"
+
+tmpfile=`mktemp`
+echo Updating page...
 sed '/src=.boxfrets.js/ {
     a <script type="text/javascript">
     r boxfrets.js
     a </script>
     d
-}' fretboard.html > page-index.html
-echo DONE
+}' fretboard.html > $tmpfile
+git checkout gh-pages
+cat $tmpfile > index.html
+echo "Page generated!"
+ls -l index.html
+echo "Commit and push to publish it!"
