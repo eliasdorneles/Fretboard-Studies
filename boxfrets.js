905aa0c17ed13430ec9d1b109765ed91f8b256b4
Elias Dorneles <eliasdorneles@gmail.com>
2011-09-19 22:53:06 -0300

    * working link creation implementation (lacks code cleanup, though)

--- boxfrets.js
+++ boxfrets.js
@@ -1,7 +1,7 @@
 /* code to handle manipulation */
 var GUITAR_STRINGS;
-var COLOR = "red";
-var POSSIBLE_COLORS = "coffee red green orange transparent"
+var COLOR = "green";
+var POSSIBLE_COLORS = ["coffee", "red", "green", "orange", "transparent"]
 var gen_fret_boxes = function(size, num_strings){
     var tr = "<tr>";
     for (var i = 0; i < size; i++){
@@ -12,29 +12,100 @@ var gen_fret_boxes = function(size, num_strings){
     for (var i = 0; i < num_strings; i++) {
 	table += tr;
     }
-    table += "</table>\n";
+    table += "</table>";
     return table;
 }
 var td_paint = function(td){
     var color = arguments.length > 1 ? arguments[1] : "coffee";
     var td = $(td);
-    td.removeClass(POSSIBLE_COLORS);
+    td.removeClass(POSSIBLE_COLORS.join(" "));
     td.addClass(color);
 }
 var td_clear = function(td){
     td_paint(td, 'transparent');
 }
+var is_painted = function(td){
+    return !$(td).hasClass('transparent');
+}
+function is_defined(obj){
+    return obj != null && obj != undefined;
+}
+/*
+ * n_string is the index of the string (starts at 0)
+ * a note may be a number, or a list with a number and a class name
+ */
+var note_paint = function(n_string, note){
+    if ($.isArray(note)){
+	td_paint(GUITAR_STRINGS[n_string][note[0]].td, note[1]);
+    } else {
+	td_paint(GUITAR_STRINGS[n_string][note].td);
+    }
+}
+/*
+ * generate representation of the diagram for url
+ */
+var uri_diagram_repr = function(guitarStrings){
+    var arr = $.map(guitarStrings, function (n, i){
+	var s = $.map(n, function (box, j){
+	    if (is_painted(box.td)){
+		var cls = $(box.td).attr('class');
+	       	return j + ":" + POSSIBLE_COLORS.indexOf(cls);
+	    }
+	    return null;
+	});
+	return s.join(",");
+    });
+    return arr.join(";");
+}
+var create_link_from_fretboard = function(){
+    var href = window.location.href;
+    if (href.indexOf('#') > 0){
+	href = href.substring(0, href.indexOf('#'));
+    }
+    href += '#';
+    href += "strings=" + uri_diagram_repr(GUITAR_STRINGS);
+    href += "&diagram_title=" + $('#diagram_title').text()
+    alert(href);
+    return href;
+}
+// handle url parameters
+function get_url_parameters(){
+    var params = {};
+    var index_params = window.location.href.indexOf('#');
+    if (index_params > 0){
+	var hashes = window.location.href.slice(index_params + 1).split('&');
+	for(var i = 0; i < hashes.length; i++){
+	    var hash = hashes[i].split('=');
+	    params[hash[0]] = hash[1];
+	}
+    }
+    return params;
+}
+function fill_from_repr(repr){
+    var a_rep = repr.split(';');
+    for(var i = 0; i < a_rep.length; i++){
+	if (is_defined(a_rep[i]) && a_rep[i] != ""){
+	    var par = $.map(a_rep[i].split(","), function (e, index){
+		var a = e.split(':');
+		return [[a[0], POSSIBLE_COLORS[a[1]]]];
+	    });
+	    show_notes_string(i, par);
+	}
+    }
+}
+// callback for when a cell is clicked
 var td_paint_or_clear = function(td, color){
     var color = arguments.length > 1 ? arguments[1] : "coffee";
     var td = $(td);
     if (!td.hasClass('transparent') && td.hasClass(color)){
-	td.removeClass(POSSIBLE_COLORS);
+	td.removeClass(POSSIBLE_COLORS.join(" "));
 	td.addClass('transparent');
     } else {
-	td.removeClass(POSSIBLE_COLORS);
+	td.removeClass(POSSIBLE_COLORS.join(" "));
 	td.addClass(color);
     }
 }
+
 var getFretcloneStrings = function(){
     var board_strings = $('#fretclone tr');
     var guitarStrings = [[], [], [], [], [], []];
@@ -53,11 +124,11 @@ var getFretcloneStrings = function(){
     }
     return guitarStrings;
 };
-var show_notes_string = function(n, notes){
+var show_notes_string = function(n_string, notes){
     if (notes != null){
 	for (var i = 0; i < notes.length; i++){
 	    if (notes[i] != null){
-		td_paint(GUITAR_STRINGS[n][notes[i]].td);
+		note_paint(n_string, notes[i]);
 	    }
 	}
     }
@@ -79,9 +150,10 @@ jQuery(function() {
 	GUITAR_STRINGS = getFretcloneStrings();
 
 	// show diagram of a C Major chord
-	show_chord([null, 1, null, 2, 3]);
+	//show_chord([null, 1, null, 2, [3, 'red']]);
 
 	var a_minor_penta = [[5, 8], [5, 8], [5, 7], [5, 7], [5, 7], [5, 8]];
 	// show diagram of an A minor penta scale form
-	show_scale(a_minor_penta);
+	//show_scale(a_minor_penta);
+	fill_from_repr(get_url_parameters()['strings']);
 });
