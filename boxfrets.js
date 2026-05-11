f60c5b016ecc29a44893e5cc7f67e01dfef1a8a7
Elias Dorneles <elias.dorneles@hey.com>
2026-05-11 21:44:41 +0200

    refactor: ditch jquery for modern vanilla JS

--- boxfrets.js
+++ boxfrets.js
@@ -18,15 +18,14 @@ var gen_fret_boxes = function(size, num_strings){
 }
 var td_paint = function(td){
     var color = arguments.length > 1 ? arguments[1] : "coffee";
-    var td = $(td);
-    td.removeClass(POSSIBLE_COLORS.join(" "));
-    td.addClass(color);
+    td.classList.remove(...POSSIBLE_COLORS);
+    td.classList.add(color);
 }
 var td_clear = function(td){
     td_paint(td, 'transparent');
 }
 var is_painted = function(td){
-    return !$(td).hasClass('transparent');
+    return !td.classList.contains('transparent');
 }
 function is_defined(obj){
     return obj != null && obj != undefined;
@@ -36,7 +35,7 @@ function is_defined(obj){
  * a note may be a number, or a list with a number and a class name
  */
 var note_paint = function(n_string, note){
-    if ($.isArray(note)){
+    if (Array.isArray(note)){
 	td_paint(GUITAR_STRINGS[n_string][note[0]].td, note[1]);
     } else {
 	td_paint(GUITAR_STRINGS[n_string][note].td);
@@ -50,15 +49,14 @@ var note_paint = function(n_string, note){
  * from note color.
  */
 var uri_diagram_repr = function(guitarStrings){
-    var arr = $.map(guitarStrings, function (n, i){
-	var s = $.map(n, function (box, j){
-	    if (is_painted(box.td)){
-		var cls = $(box.td).attr('class');
-	       	return j + ":" + POSSIBLE_COLORS.indexOf(cls);
-	    }
-	    return null;
-	});
-	return s.join(",");
+    var arr = guitarStrings.map(function(n){
+        var s = n.reduce(function(acc, box, j){
+            if (is_painted(box.td)){
+                acc.push(j + ":" + POSSIBLE_COLORS.indexOf(box.td.className));
+            }
+            return acc;
+        }, []);
+        return s.join(",");
     });
     return arr.join(";");
 }
@@ -69,12 +67,12 @@ var create_link_from_fretboard = function(){
     }
     href += '#';
     href += "strings=" + uri_diagram_repr(GUITAR_STRINGS);
-    href += "&diagram_title=" + escape($('#diagram_title').val());
+    href += "&diagram_title=" + escape(document.getElementById('diagram_title').value);
     return href;
 }
 var update_link = function(){
-	    $('#linkthis').attr('href', create_link_from_fretboard());
-	    $('#linkquiz').attr('href', create_link_from_fretboard() + "&q=y");
+    document.getElementById('linkthis').href = create_link_from_fretboard();
+    document.getElementById('linkquiz').href = create_link_from_fretboard() + "&q=y";
 }
 // handle url parameters
 function get_url_parameters(){
@@ -95,14 +93,14 @@ function fill_from_repr(repr){
     var match_color = arguments.length > 1 ? arguments[1] : null;
     for(var i = 0; i < a_rep.length; i++){
 	if (is_defined(a_rep[i]) && a_rep[i] != ""){
-	    var par = $.map(a_rep[i].split(","), function (e, index){
+	    var par = a_rep[i].split(",").reduce(function(acc, e){
 		var a = e.split(':');
 		var color = POSSIBLE_COLORS[a[1]];
-		if (match_color != null && color != match_color){
-		    return null;
+		if (match_color == null || color === match_color){
+		    acc.push([a[0], color]);
 		}
-		return [[a[0], color]];
-	    });
+		return acc;
+	    }, []);
 	    show_notes_string(i, par);
 	}
     }
@@ -110,39 +108,38 @@ function fill_from_repr(repr){
 // callback for when a cell is clicked
 var td_paint_or_clear = function(td, color){
     var color = arguments.length > 1 ? arguments[1] : "coffee";
-    var td = $(td);
-    if (!td.hasClass('transparent') && td.hasClass(color)){
-	td.removeClass(POSSIBLE_COLORS.join(" "));
-	td.addClass('transparent');
+    if (!td.classList.contains('transparent') && td.classList.contains(color)){
+	td.classList.remove(...POSSIBLE_COLORS);
+	td.classList.add('transparent');
     } else {
-	td.removeClass(POSSIBLE_COLORS.join(" "));
-	td.addClass(color);
+	td.classList.remove(...POSSIBLE_COLORS);
+	td.classList.add(color);
     }
 }
 
 var getFretcloneStrings = function(){
-    var board_strings = $('#fretclone tr');
+    var board_strings = document.querySelectorAll('#fretclone tr');
     var guitarStrings = [[], [], [], [], [], []];
     for (var i = 0; i < board_strings.length; i++){
-		var children = board_strings[i].children;
-		for (var j = 0; j < children.length; j++){
-		    var box = {
-				td: children[j],
-				paint: function(){
-				    td_paint_or_clear(this, COLOR);
-				},
-		    }
-		    guitarStrings[i].push(box);
-		    $(children[j]).click(box.paint);
+	var children = board_strings[i].children;
+	for (var j = 0; j < children.length; j++){
+	    var box = {
+		td: children[j],
+		paint: function(){
+		    td_paint_or_clear(this, COLOR);
+		},
+	    }
+	    guitarStrings[i].push(box);
+	    children[j].addEventListener('click', box.paint);
 
-		    // eraser:
-		    $(children[j]).mouseover(function(){
-		    	if (ERASER == true){
-	    			td_clear(this);
-	    		}
-	    		update_link();
-	    	});
+	    // eraser:
+	    children[j].addEventListener('mouseover', function(){
+		if (ERASER == true){
+		    td_clear(this);
 		}
+		update_link();
+	    });
+	}
     }
     return guitarStrings;
 };
@@ -170,112 +167,119 @@ var show_scale = function (scale){
 var check_answers = function(a_repr) {
     var a_rep = a_repr.split(';');
     for(var i = 0; i < a_rep.length; i++){
-		if (is_defined(a_rep[i]) && a_rep[i] != ""){
-		    var marked_notes = $.map(a_rep[i].split(","), function (e, index){
-		        return [e.split(":")[0]];
-		    });
-		    for (var j = 0; j < GUITAR_STRINGS[i].length; j++){
-				if ($.inArray(j.toString(), marked_notes) >= 0){
-				    var color = is_painted(GUITAR_STRINGS[i][j].td) ? "green" : "orange";
-				    note_paint(i, [j, color]);
-				} else {
-				    if (is_painted(GUITAR_STRINGS[i][j].td)){
-					// show wrong notes in red
-					note_paint(i, [j, "red"]);
-				    }
-				}
+	if (is_defined(a_rep[i]) && a_rep[i] != ""){
+	    var marked_notes = a_rep[i].split(",").map(function(e){
+		return e.split(":")[0];
+	    });
+	    for (var j = 0; j < GUITAR_STRINGS[i].length; j++){
+		if (marked_notes.indexOf(j.toString()) >= 0){
+		    var color = is_painted(GUITAR_STRINGS[i][j].td) ? "green" : "orange";
+		    note_paint(i, [j, color]);
+		} else {
+		    if (is_painted(GUITAR_STRINGS[i][j].td)){
+			// show wrong notes in red
+			note_paint(i, [j, "red"]);
 		    }
 		}
+	    }
 	}
+    }
 }
 var clear_fretboard = function(){
-	for(var i = 0; i < GUITAR_STRINGS.length; i++){
-		for(var j = 0; j < GUITAR_STRINGS[i].length; j++){
-			td_clear(GUITAR_STRINGS[i][j].td);
-		}
+    for(var i = 0; i < GUITAR_STRINGS.length; i++){
+	for(var j = 0; j < GUITAR_STRINGS[i].length; j++){
+	    td_clear(GUITAR_STRINGS[i][j].td);
 	}
+    }
 }
 var pick_note = function(position, offset){
-	if(typeof(offset)==='undefined') offset=0; // defaults to E
-	if(typeof(position)==='undefined') position=4; // defaults to E
-	var scale = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
-	return scale[(position + offset) % scale.length];
+    if(typeof(offset)==='undefined') offset=0; // defaults to E
+    if(typeof(position)==='undefined') position=4; // defaults to E
+    var scale = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
+    return scale[(position + offset) % scale.length];
 }
 var set_notes = function(){
-	//var offsets = [4, 9, 2, 7, 11, 4];
-	var offsets = [4, 11, 7, 2, 9, 4];
-	for(var i = 0; i < GUITAR_STRINGS.length; i++){
-		for(var j = 0; j < GUITAR_STRINGS[i].length; j++){
-			$(GUITAR_STRINGS[i][j].td).find('.note').html(pick_note(j, offsets[i]));
-		}
+    //var offsets = [4, 9, 2, 7, 11, 4];
+    var offsets = [4, 11, 7, 2, 9, 4];
+    for(var i = 0; i < GUITAR_STRINGS.length; i++){
+	for(var j = 0; j < GUITAR_STRINGS[i].length; j++){
+	    GUITAR_STRINGS[i][j].td.querySelector('.note').innerHTML = pick_note(j, offsets[i]);
 	}
+    }
 }
 var loadFromUrl = function(url_params){
-	if (is_defined(url_params['diagram_title'])){
-		$('#diagram_title').val(unescape(url_params['diagram_title']));
+    if (is_defined(url_params['diagram_title'])){
+	document.getElementById('diagram_title').value = unescape(url_params['diagram_title']);
+    }
+    if (is_defined(url_params['strings'])){
+	if (is_defined(url_params['q']) && url_params['q'] == 'y'){
+	    COLOR = "coffee";
+	    document.getElementById('checkanswer').addEventListener('click', function(){
+		check_answers(url_params['strings']);
+	    });
+	    document.getElementById('colorchooser').style.display = 'none';
+	    document.getElementById('checkanswer').style.display = '';
+	} else {
+	    fill_from_repr(url_params['strings']);
 	}
-	if (is_defined(url_params['strings'])){
-		if (is_defined(url_params['q']) && url_params['q'] == 'y'){
-			COLOR = "coffee";
-			$('#checkanswer').click(function(){
-				check_answers(url_params['strings']);
-			});
-			$('#colorchooser').hide();
-			$('#checkanswer').show();
-		} else {
-			fill_from_repr(url_params['strings']);
-		}
-	}
+    }
 }
-jQuery(function() {
-	$(gen_fret_boxes(19, 6)).insertAfter($('#mainfretboard'));
-	GUITAR_STRINGS = getFretcloneStrings();
-	set_notes();
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('mainfretboard').insertAdjacentHTML('afterend', gen_fret_boxes(19, 6));
+    GUITAR_STRINGS = getFretcloneStrings();
+    set_notes();
 
-	// show diagram of a C Major chord
-	//show_chord([null, 1, null, 2, [3, 'red']]);
+    // show diagram of a C Major chord
+    //show_chord([null, 1, null, 2, [3, 'red']]);
 
-	//var a_minor_penta = [[5, 8], [5, 8], [5, 7], [5, 7], [5, 7], [5, 8]];
-	// show diagram of an A minor penta scale form
-	//show_scale(a_minor_penta);
-	loadFromUrl(get_url_parameters());
-	update_link();
-	var message = $('#message');
-	// update link at every click on a note...
-	$('#fretclone tr td').click(update_link);
-	// or when changes are made to the title
-	$('#diagram_title').change(update_link)
-	    .keyup(update_link)
-	    .keydown(update_link);
-	$('#diagram_title').click(function(){ this.select(); });
+    //var a_minor_penta = [[5, 8], [5, 8], [5, 7], [5, 7], [5, 7], [5, 8]];
+    // show diagram of an A minor penta scale form
+    //show_scale(a_minor_penta);
+    loadFromUrl(get_url_parameters());
+    update_link();
+    var message = document.getElementById('message');
+    // update link at every click on a note...
+    document.querySelectorAll('#fretclone tr td').forEach(function(td){ td.addEventListener('click', update_link); });
+    // or when changes are made to the title
+    var titleInput = document.getElementById('diagram_title');
+    ['change', 'keyup', 'keydown'].forEach(function(ev){ titleInput.addEventListener(ev, update_link); });
+    titleInput.addEventListener('click', function(){ this.select(); });
 
-	// set up color buttons according each element's class
-	$('.color_button').click(function(){
-		COLOR = $(this).attr('class').split(' ')[1];
-		message.html('');
-	})
-
-	// set up eraser and clear buttons:
-	$('#eraser').toggle(function(){
-		ERASER = true;
-		$(this).removeClass('blank').addClass('white');
-		message.html('<b>Eraser activated!</b><br /><sub>Mouseover the marks to erase them</sub>')
-	}, function(){
-		$(this).removeClass('white').addClass('blank');
-		ERASER = false;
-		message.html('')
+    // set up color buttons according each element's class
+    document.querySelectorAll('.color_button').forEach(function(btn){
+	btn.addEventListener('click', function(){
+	    COLOR = this.className.split(' ')[1];
+	    message.innerHTML = '';
 	});
-	$('#clear').click(function(){
-		message.html('');
-		clear_fretboard(); update_link();
-	});
+    });
 
-	// set up example links
-	$('#examples ul li a').click(function(){
-		clear_fretboard();
-		loadFromUrl(get_url_parameters($(this).attr('href')));
-		update_link();
+    // set up eraser and clear buttons:
+    document.getElementById('eraser').addEventListener('click', function(){
+	ERASER = !ERASER;
+	if (ERASER) {
+	    this.classList.remove('blank');
+	    this.classList.add('white');
+	    message.innerHTML = '<b>Eraser activated!</b><br /><sub>Mouseover the marks to erase them</sub>';
+	} else {
+	    this.classList.remove('white');
+	    this.classList.add('blank');
+	    message.innerHTML = '';
+	}
+    });
+    document.getElementById('clear').addEventListener('click', function(){
+	message.innerHTML = '';
+	clear_fretboard(); update_link();
+    });
+
+    // set up example links
+    document.querySelectorAll('#examples ul li a').forEach(function(a){
+	a.addEventListener('click', function(e){
+	    e.preventDefault();
+	    clear_fretboard();
+	    loadFromUrl(get_url_parameters(this.getAttribute('href')));
+	    update_link();
 	});
-	// TODO: fix quiz behavior (do not show note names in quiz mode), and show message on check answer
-	// TODO: find a way to show enarmonics
+    });
+    // TODO: fix quiz behavior (do not show note names in quiz mode), and show message on check answer
+    // TODO: find a way to show enarmonics
 });
